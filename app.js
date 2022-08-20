3dac53bf7db80559177651ba7bd5b73f691c2e6f
Sushant Kadam <sushantkadam15@gmail.com>
2022-08-20 14:34:42 -0400

    index.html Added, Bootstrap Added, Initial Page Created

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const port = 3200;
 const path = require('path');
 
 //********** Serving Static Files **********//
-app.use(express.static('public'));
-app.use('public', express.static(__dirname + '/public'));
+app.use(express.static('public'))
 
 //********** Embedded JS **********// 
 app.set('view engine', 'ejs');
@@ -13,5 +12,5 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.listen(port, () => console.log(`Weather App active on ${port}`));
 app.get('/', (req, res) => {
-    res.render('app');
+    res.render('index');
 })
